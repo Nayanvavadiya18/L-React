fe604ee7f889cbdfdf7e213f3d51a7a0e2519ad5
Nayanvavadiya <nayanvavdiya062@gmail.com>
2026-02-01 13:45:23 +0530

    Added API call (Task 3)

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -23,10 +23,11 @@ function Users() {
 
       <button onClick={getData}>Get Data</button>
 
+      <h4>User Name - User Email - User City - User Phone</h4>
       {show &&
         users.map((user) => (
           <p key={user.id}>
-            {user.name} - {user.email}
+            {user.name} -   {user.email} - {user.address.city} - {user.phone}
           </p>
         ))}
     </div>
